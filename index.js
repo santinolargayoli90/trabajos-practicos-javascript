581422a8dd378d052c65685362fffda25901a952
santinolargayoli90 <166016530+santinolargayoli90@users.noreply.github.com>
2024-04-04 08:27:17 -0300

    agregar archivos iniciales

---
+++ index.js
@@ -0,0 +1,33 @@
+console.log("la suma es" + 1+1)
+console.log("La resta es" + 6-2)
+console.log("la multiplicacion es" + 4*2)
+console.log("la division es" + 4/2)
+//punto 3
+let numero1=2
+let numero2=4 
+let numero3=2
+let numero4=8
+alert(numero1)
+alert(numero2)
+alert(numero3)
+alert(numero4)
+//punto 4
+let añoactual=2024
+let añonacimiento=2009
+alert("la edad es"+ añoactual - añonacimiento)
+//punto 7 
+let nombrealumno = "santino"
+let apellidoalumno = "Largayoli"
+console.log ("bienvenido "+ nombrealumno + apellidoalumno)
+alert ("bienvenido" + nombrealumno+apellidoalumno)
+//punto 5
+ añoactual=prompt("ingresa el año actual")
+ añonacimiento=prompt("ingresa tu año de nacimiento")
+// punto 6 
+let nombre = prompt ("ingresa tu nombre ")
+alert ( "bienvenido" + nombre)
+//punto 9
+let diez =10 
+let tres =3
+let resultadomultiplicacion=10*3
+console.log (resultadomultiplicacion)
